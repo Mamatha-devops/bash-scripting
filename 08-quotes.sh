8444802d81b84c0aebf37edf76b9ddd72fbae9ee
Mamatha-devops <mammu.vangeti@gmail.com>
2025-04-11 03:32:57 -0400

    updated the repo

---
+++ 08-quotes.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+# ""   : Double Quotes 
+# ''   : Single Quotes   : # Single Quotes Always Eliminates The Power Of The Special Variable 
+
+a=100 
+echo "Demo on quotes:"
+echo $?
+echo '$?'
+echo "Value of is $?" 
+echo "Value of a is $a"
+echo 'Value of a is $a'
